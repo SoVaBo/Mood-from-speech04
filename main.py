7b10e92f5820b850974e77e71f04d0933d87726f
SoroushBorzoo <94343205+SoVaBo@users.noreply.github.com>
2022-03-09 06:47:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Mood from Audio File")
+st.subheader("We try analyze audio file based on word and say if it is positive or not!")
+st.write('Please upload your audio file')
